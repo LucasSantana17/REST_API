d26aecc6b58e0d467b0b31e44e69964b79b13953
Lucas <sjoaolucascostasantana@gmail.com>
2024-04-10 20:58:46 -0300

    front-novo

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 const express = require('express');
-<<<<<<< HEAD
-const routesCadastro = require('./routes/cadastro')
+const app = express();
 const cors = require('cors');
-const app = express();
-
-
-app.use(cors());
-app.use('/',routesCadastro)
-=======
-const app = express();
-const routesCadastro = require('./routes/cadastro');
+const routesCadastro = require('./routes/cadastro')
 const rota_banco = require('./routes/cadastro_db');
 
 
-app.use('/user', rota_banco);
-
->>>>>>> ed02d2aac00e06a60c54944d1e25149cce54cc32
+app.use(cors());
+app.use('/',routesCadastro);
+app.use('/', rota_banco);
 app.use('/cadastro', routesCadastro);
 app.use("/user",(request, response, next) =>{
         response.status(200).send({
